010e490dabdf2ee7031ca6de8d76892043709022
Francis <francispham89@gmail.com>
2018-11-14 21:51:16 -0800

    Create Basic Node Server

---
+++ app.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url, req.method, req.headers);
+    // process.exit();
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<header><title>Enter Message</title></header>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send Now</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            // fs.writeFileSync('message.txt', message); })
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    res.setHeader('Context-Type', 'text/html');
+    res.write('<html>');
+    res.write('<header><title>My Node Page</title></header>');
+    res.write('<body><h1>Hello from my node server</h1></body>');
+    res.write('</html>');
+    res.end();
+
+});
+
+server.listen(3000);
