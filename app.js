aa37d8f915008ce89cdefbfc047e56fa8cd1bc6a
Francis <francispham89@gmail.com>
2018-11-15 12:57:30 -0800

    Handle Routes w/ Express

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
 
 
 app.use((req, res, next) => {
-    console.log('In the middleware!');
+    console.log('This always runs!');
     next(); // Allows the request to continue to the next middleware in line
 });
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+
+app.use('/add-product',(req, res, next) => {
+    res.send(
+        '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'
+        );
+});
+
+app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
     res.send('<h1>Hello From Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
