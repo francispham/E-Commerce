8ee334dab154f0cf063b7f696801886f9b313773
Francis <francispham89@gmail.com>
2018-11-15 22:23:32 -0800

    Implement Pug

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const rootDir = require('./util/path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const shopRoutes = require('./routes/shop');
 const adminData = require('./routes/admin');
 
@@ -23,7 +26,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: "Page Not Found!"});
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,14 +10,15 @@ const products = [];
 // Because using app.use('/admin', adminRoutes) in app.js. These 2 routes will be come:
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
     });
     
 // /admin/add-product => POST
 // Using "post" for "use" will get into this route accidentally:
 router.post('/add-product', (req, res, next) => {
     // console.log(req.body);
-    products.push({ title: req.body.title })
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
