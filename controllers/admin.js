8c3e78e93c3ca2c3b5e4174bf313c0faad62c8f8
Francis <francispham89@gmail.com>
2018-12-05 13:14:18 -0800

    Edit the Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res, next) => {
     const description = req.body.description;
     // Got moved to models:
     // products.push({ title: req.body.title });
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 };
@@ -42,7 +42,20 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req, res, next) => {
-    
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product (
+        prodId,
+        updatedTitle,
+        updatedImageUrl,
+        updatedPrice,
+        updatedDesc
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
