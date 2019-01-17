6ff18ad3028e285958f1db40ca66d2a7fe16c194
Francis <francispham89@gmail.com>
2019-01-16 23:07:58 -0800

    Implementing Deleting Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -131,6 +131,17 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
+    /* Before Sequelize:
     Product.deleteById(prodId);
     res.redirect('/admin/products');
+    */
+    Product.findById(prodId)
+        .then(product => {
+            return product.destroy();
+        })
+        .then(result => {
+            console.log('DESTROYED PRODUCT');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 };
