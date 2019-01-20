f178f10873c3653b9b573b6dc9569453a4dc6f14
Francis <francispham89@gmail.com>
2019-01-19 17:57:38 -0800

    Implementing One-To-Many & Many-To-Many Relations with Cart & Cart-item

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ const sequelize = require('./util/database');
 const path = require('path');
 const rootDir = require('./util/path');
 
-// Add Models:
+// Add Models for Associations:
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 
 const app = express();
 
@@ -93,6 +96,10 @@ app.use(errorController.get404);
 // Use Models to define Relationship:
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
     /* Before Adding Dummy User:
@@ -103,7 +110,7 @@ sequelize
         app.listen(3000);
     })
     */
-    .sync()
+    .sync({ force: true })
     .then(result => {
         return User.findById(1);
     })
