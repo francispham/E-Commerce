cfc3a64739cf683b403d0180f6ae54b6d2afca24
Francis <francispham89@gmail.com>
2019-01-21 00:00:36 -0800

    Add Route & Controller for post an order

--- controllers/shop.js
+++ controllers/shop.js
@@ -239,6 +239,8 @@ exports.postCartDeleteProduct = (req, res, next) => {
         .catch(err => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {};
+
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
         path: '/orders',
